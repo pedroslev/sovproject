9b555668667f515410e1ddbd137f5488692e73e6
pedroslevv <pedro.lopez@philips.edu.ar>
2020-07-23 18:36:20 -0300

    browserify with required shit

--- build/Server/database.js
+++ build/Server/database.js
@@ -1,10 +1,9 @@
-<script>
 const { Client } = require('pg');
 const connectionString ='postgres://nknskzkksjrswn:3db4897d9bb441379bf69eb55e105e6f200aa316b9d762379c3285abb13d2493@ec2-18-211-48-247.compute-1.amazonaws.com:5432/d5nviljvls6e8v';
 
 const client = new client(connectionString);
 
 client.connect();
-</script>
+
 
 
