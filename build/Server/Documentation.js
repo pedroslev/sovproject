139d17b2d961677a5e018069755f95f90a0fef08
pedroslevv <pedro.lopez@philips.edu.ar>
2020-07-23 17:15:00 -0300

    DATABASE DOC

--- build/Server/Documentation.js
+++ build/Server/Documentation.js
@@ -1,11 +1,26 @@
-// Como hacer una consulta
-$query = 'SELECT * from ... etc';
+//DOCUMENTACION PARA POSTGRES SQL DATABASE
 
-connection.query($query, function(err, rows, fields) {
-    if(err){
-        console.log("An error ocurred performing the query.");
-        return;
-    }
+//LAS QUERYS SON IGUAL A SQL, PERO SE LE DEBE DAR PERMISO LECTURA Y ESCRITURA A LA TRANSACCION Y REALIZAR COMIT LUEGO DE CADA UNA
 
-    console.log("Query succesfully executed: ", rows);
-});
+//EJEMPLO DE QUERY PARA CREAR LA TABLA DE PRODUCTOS
+set transaction read write;
+CREATE TABLE Productos (
+    ID int,
+    Codigo varchar(255),
+    Nombre varchar(255),
+    Descripcion varchar(255),
+    Cantidad int,
+    Categoria varchar(255),
+    Proveedor varchar(255),
+    LinkFoto varchar(255),
+    Costo int,
+    Margen int,
+    Precio int,
+    Mostrar int
+);
+commit;
+
+//EJEMPLO DE QUERY PARA INSERTAR VALORES EN DICHA TABLAs
+set transaction read write;
+INSERT INTO Productos (ID, Codigo, Nombre, Descripcion, Cantidad, Categoria, Proveedor, LinkFoto, Costo, Margen, Precio, Mostrar)
+VALUES ('1', 'ABC', 'Test', 'Esto es un test', '10', 'testing', 'haze', 'NULL', '10', '100', '20', '1');
