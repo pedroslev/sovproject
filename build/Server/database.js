a0566a29ef9682b7eb1d0db1fc6670b148036225
pedroslevv <pedro.lopez@philips.edu.ar>
2020-07-29 11:50:54 -0300

    Update database.js

--- build/Server/database.js
+++ build/Server/database.js
@@ -1,13 +1,4 @@
-<?php
-
-    $db_connection = pg_connect("host=ec2-18-211-48-247.compute-1.amazonaws.com dbname=d5nviljvls6e8v user=nknskzkksjrswn password=3db4897d9bb441379bf69eb55e105e6f200aa316b9d762379c3285abb13d2493");
-
-    $result = pg_query($db_connection, "SELECT * FROM Productos");
-    
-?>
-
-<script>
-
-    var resultado = $result;
-    console.log(resultado);
-</script>
+var pg = require('pg');
+var connectionString = "postgres://nknskzkksjrswn:3db4897d9bb441379bf69eb55e105e6f200aa316b9d762379c3285abb13d2493@ec2-18-211-48-247.compute-1.amazonaws.com:5432/d5nviljvls6e8v";
+var pgClient = new pg.Client(connectionString);
+pgClient.connect();
